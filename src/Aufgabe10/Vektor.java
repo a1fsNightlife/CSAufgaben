641829258284954668d95eca945ceb85b114e0f1
a1fsNightlife <81155925+a1fsNightlife@users.noreply.github.com>
2021-03-24 14:41:24 +0100

    add logic to Aufgabe10 & Aufgabe10Test

--- src/Aufgabe10/Vektor.java
+++ src/Aufgabe10/Vektor.java
@@ -2,9 +2,9 @@ package Aufgabe10;
 
 public class Vektor {
 
-    private double x;
-    private double y;
-    private double z;
+    private final double x;
+    private final double y;
+    private final double z;
 
     public Vektor(double x, double y, double z) {
         this.x = x;
@@ -12,28 +12,49 @@ public class Vektor {
         this.z = z;
     }
 
-    public double getX() {
-        return x;
+    public static Vektor additionVektor(Vektor vektor1, Vektor vektor2) {
+        Double returnVektorX = vektor1.getX() + vektor2.getX();
+        Double returnVektorY = vektor1.getY() + vektor2.getY();
+        Double returnVektorZ = vektor1.getZ() + vektor2.getZ();
+        return new Vektor(returnVektorX, returnVektorY, returnVektorZ);
     }
 
-    public void setX(double x) {
-        this.x = x;
+    public static Vektor subtractVektor(Vektor vektor1, Vektor vektor2) {
+        Double returnVektorX = vektor1.getX() - vektor2.getX();
+        Double returnVektorY = vektor1.getY() - vektor2.getY();
+        Double returnVektorZ = vektor1.getZ() - vektor2.getZ();
+        return new Vektor(returnVektorX, returnVektorY, returnVektorZ);
+    }
+
+    public static Vektor crossProductOfVektors(Vektor vektor1, Vektor vektor2) {
+        Double returnVektorX = ((vektor1.getY() * vektor2.getZ()) - (vektor1.getZ() * vektor2.getY()));
+        Double returnVektorY = ((vektor1.getZ() * vektor2.getX()) - (vektor1.getX() * vektor2.getZ()));
+        Double returnVektorZ = ((vektor1.getX() * vektor2.getY()) - (vektor1.getY() * vektor2.getX()));
+        return new Vektor(returnVektorX, returnVektorY, returnVektorZ);
+    }
+
+    public static Vektor multiplyVektorWithScalar(Vektor vektor1, Double scalar) {
+        return new Vektor(vektor1.getX() * scalar,
+                    vektor1.getY() * scalar,
+                    vektor1.getZ() * scalar);
+    }
+
+    public static Vektor divideVektorWithScalar(Vektor vektor1, Double scalar) {
+        return new Vektor(vektor1.getX() / scalar,
+                    vektor1.getY() / scalar,
+                    vektor1.getZ() / scalar);
+    }
+
+    public double getX() {
+        return x;
     }
 
     public double getY() {
         return y;
     }
 
-    public void setY(double y) {
-        this.y = y;
-    }
-
     public double getZ() {
         return z;
-    }
-
-    public void setZ(double z) {
-        this.z = z;
     }
 
     @Override
--- src/Test/VektorTest.java
+++ src/Test/VektorTest.java
@@ -1,14 +1,70 @@
 package Test;
 
+import Aufgabe10.Vektor;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class VektorTest {
 
-    @Test
-    public void shouldAddCorrectly() {
-        Double x;
-        Double y;
-        Double z;
+    private static final double DELTA = 0.001;
 
+    @Test
+    public void shouldAdditionCorrectly() {
+        Vektor vektor1 = new Vektor(10.0, 20.0, 25.0);
+        Vektor vektor2 = new Vektor(5.0, 30.0, 45.0);
+        Vektor expected = new Vektor(15.0, 50.0, 70.0);
+        Vektor result = Vektor.additionVektor(vektor1, vektor2);
+
+        assertEquals(expected.getX(), result.getX(), DELTA);
+        assertEquals(expected.getY(), result.getY(), DELTA);
+        assertEquals(expected.getZ(), result.getZ(), DELTA);
+    }
+
+    @Test
+    public void shouldSubtractCorrectly() {
+        Vektor vektor1 = new Vektor(25.0, 50.0, 2.0);
+        Vektor vektor2 = new Vektor(4.0, 5.0, 1.0);
+        Vektor expected = new Vektor(21.0, 45.0, 1.0);
+        Vektor result = Vektor.subtractVektor(vektor1, vektor2);
+
+        assertEquals(expected.getX(), result.getX(), DELTA);
+        assertEquals(expected.getY(), result.getY(), DELTA);
+        assertEquals(expected.getZ(), result.getZ(), DELTA);
+    }
+
+    @Test
+    public void shouldCalcCrossProductCorrectly() {
+        Vektor vektor1 = new Vektor(25.0, 50.0, 2.0);
+        Vektor vektor2 = new Vektor(4.0, 5.0, 1.0);
+        Vektor expected = new Vektor(40.0, -17.0, -75.0);
+        Vektor result = Vektor.crossProductOfVektors(vektor1, vektor2);
+
+        assertEquals(expected.getX(), result.getX(), DELTA);
+        assertEquals(expected.getY(), result.getY(), DELTA);
+        assertEquals(expected.getZ(), result.getZ(), DELTA);
+    }
+
+    @Test
+    public void shouldMultiplyWithScalar() {
+        Vektor vektor = new Vektor(25.0, 50.0, 34.0);
+        Double scalar = 17.0;
+        Vektor expected = new Vektor(425.0, 850.0, 578.0);
+        Vektor result = Vektor.multiplyVektorWithScalar(vektor, scalar);
+
+        assertEquals(expected.getX(), result.getX(), DELTA);
+        assertEquals(expected.getY(), result.getY(), DELTA);
+        assertEquals(expected.getZ(), result.getZ(), DELTA);
+    }
+
+    @Test
+    public void shouldDivideWithScalar() {
+        Vektor vektor = new Vektor(25.0, 50.0, 34.0);
+        Double scalar = 5.0;
+        Vektor expected = new Vektor(5.0, 10.0, 6.8);
+        Vektor result = Vektor.divideVektorWithScalar(vektor, scalar);
+
+        assertEquals(expected.getX(), result.getX(), DELTA);
+        assertEquals(expected.getY(), result.getY(), DELTA);
+        assertEquals(expected.getZ(), result.getZ(), DELTA);
     }
 }
